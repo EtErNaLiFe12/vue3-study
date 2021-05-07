3fcc220e3c71dba61c6fd6ac5915407516437e65
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-05-07 19:58:42 +0900

    vue3-computed

---
+++ src/App.vue
@@ -0,0 +1,105 @@
+<!-- <template>
+  <Fruits />
+</template>
+
+<script>
+import Fruits from '~/components/Fruits'
+export default {
+  components: {
+    Fruits
+  }
+}
+</script> -->
+
+<!-- -------------computed(계산된 데이터) 캐싱 / Getter / Setter--------------------------- -->
+
+<!--
+<template>
+  <button @click="add">
+    ADD
+  </button>  
+  <h1>{{ msg.split('').reverse().join('') }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // GETTER(값을 얻어내는 용도), SETTER (값을 지정하는 용도)
+      msg: 'Hello Computed!'
+    }
+  },
+  computed: {
+    // GETTER - 값을 얻어내는 용도
+    // reversedMessage() {
+    //   return this.msg.split('').reverse().join('')
+    // }
+    // Getter, Setter
+    reversedMessage: {
+      get() {
+        return this.msg.split('').reverse().join('')
+      },
+      set(newValue) {
+        this.msg = newValue
+      }
+    }
+  },
+  // methods: {
+  //   reverseMessage() {
+  //     return this.msg.split('').reverse().join('')
+  //   }
+  // }
+  methods: {
+    add() {
+      this.reversedMessage += '!?'
+      // this.msg += '!?'
+    }
+  }
+}
+</script>
+-->
+
+<!-- -----------------watch--------------------- -->
+ 
+<template>
+  <h1 @click="changeMessage"> 
+    <!-- v-on:click => @click -->
+    {{ msg }}
+  </h1>
+  <h1>{{ reversedMessage }}</h1>
+  <!-- <h1>{{ changeMessage() }}</h1> -->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello?'
+    }
+  },
+  computed: {
+    reversedMessage() {
+      return this.msg.split('').reverse().join('')
+    }
+  },
+  watch: { // 변경을 감시하는 부분
+    msg(nv) { // method () 괄호 안에 명칭을 추가해도 됨 (원하는 이름으로 설정 가능)
+      console.log('msg:', nv)
+    },
+    reversedMessage() {
+      console.log('reversedMessage:', this.reversedMessage)
+    }
+  },
+  methods: {
+    changeMessage() {
+      this.msg = 'Good!'
+    }
+  }
+}
+</script>
+
+<!-- ---------------------------------------------- -->
---
+++ src/components/Fruits.vue
@@ -0,0 +1,48 @@
+<!-- <template>
+  <section v-if="hasFruit">
+    <h1>
+      Fruits
+      <ul>
+        <li
+          v-for="fruit in fruits"
+          :key="fruit">
+          {{ fruit }}
+        </li>
+      </ul>
+    </h1>
+  </section>
+  <section>
+    <h1>Reverse Fruits</h1>
+    <ul>
+      <li
+        v-for="fruit in reverseFruits"
+        :key="fruit">
+        {{ fruit }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fruits: [
+        'Apple', 'Banana', 'Cherry'
+      ]
+    }
+  },
+  computed: {
+    hasFruit() {
+      return this.fruits.length > 0
+    },
+    reverseFruits() {
+      return this.fruits.map(fruit => {
+        // 'Apple' => ['A','p','p','l','e']
+        // => ['e','l','p','p','A'] => 'elppA'
+        return fruit.split('').reverse().join('')
+      })
+    }
+  }
+}
+</script> -->
