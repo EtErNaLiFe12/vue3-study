2c8aa92bea662df0e0d0d065f3a527fe2f476118
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-05-07 19:56:58 +0900

    vue3-syntax upload

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- <h1 
+    v-once
+    @click="add">
+    {{ msg }}
+  </h1>
+  <h1 v-html="msg"></h1> -->
+  <h1
+    :[attr]="'active'"
+    @[event]="add">
+    <!--v-bind: 으로 데이터를 삽입 가능  -->
+    {{ msg }}
+  </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // msg: '<div style="color: red;">Hello!!</div>'
+      msg: 'active',
+      attr: 'class',
+      event: 'click'
+    }
+  },
+  methods: {
+    add() {
+      this.msg += '!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.active {
+  color: royalblue;
+  font-size: 100px;
+}
+</style>
